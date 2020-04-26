c3485fe7d325630f6a3ffa029bac93e7ad48b512
Othmen mohamed ali <mohamedali.othmen.simplon@gmail.com>
2020-04-26 18:02:45 +0100

    create login, register and shopping pages

---
+++ doshop.js
@@ -0,0 +1,273 @@
+var shoppingCart = (function() {
+   
+    cart = [];
+    
+    // Constructor
+    function Item(name, price, count) {
+      this.name = name;
+      this.price = price;
+      this.count = count;
+    }
+    
+    // Save cart
+    function saveCart() {
+      sessionStorage.setItem('shoppingCart', JSON.stringify(cart));
+    }
+    
+      // Load cart
+    function loadCart() {
+      cart = JSON.parse(sessionStorage.getItem('shoppingCart'));
+    }
+    if (sessionStorage.getItem("shoppingCart") != null) {
+      loadCart();
+    }
+    
+  
+    // =============================
+    // Public methods and propeties
+    // =============================
+    var obj = {};
+    
+    // Add to cart
+    obj.addItemToCart = function(name, price, count) {
+      for(var item in cart) {
+        if(cart[item].name === name) {
+          cart[item].count ++;
+          saveCart();
+          return;
+        }
+      }
+      var item = new Item(name, price, count);
+      cart.push(item);
+      saveCart();
+    }
+    // Set count from item
+    obj.setCountForItem = function(name, count) {
+      for(var i in cart) {
+        if (cart[i].name === name) {
+          cart[i].count = count;
+          break;
+        }
+      }
+    };
+    // Remove item from cart
+    obj.removeItemFromCart = function(name) {
+        for(var item in cart) {
+          if(cart[item].name === name) {
+            cart[item].count --;
+            if(cart[item].count === 0) {
+              cart.splice(item, 1);
+            }
+            break;
+          }
+      }
+      saveCart();
+    }
+  
+    // Remove all items from cart
+    obj.removeItemFromCartAll = function(name) {
+      for(var item in cart) {
+        if(cart[item].name === name) {
+          cart.splice(item, 1);
+          break;
+        }
+      }
+      saveCart();
+    }
+  
+    // Clear cart
+    obj.clearCart = function() {
+      cart = [];
+      saveCart();
+    }
+  
+    // Count cart 
+    obj.totalCount = function() {
+      var totalCount = 0;
+      for(var item in cart) {
+        totalCount += cart[item].count;
+      }
+      return totalCount;
+    }
+  
+    // Total cart
+    obj.totalCart = function() {
+      var totalCart = 0;
+      for(var item in cart) {
+        totalCart += cart[item].price * cart[item].count;
+      }
+      return Number(totalCart.toFixed(2));
+    }
+  
+    // List cart
+    obj.listCart = function() {
+      var cartCopy = [];
+      for(i in cart) {
+        item = cart[i];
+        itemCopy = {};
+        for(p in item) {
+          itemCopy[p] = item[p];
+  
+        }
+        itemCopy.total = Number(item.price * item.count).toFixed(2);
+        cartCopy.push(itemCopy)
+      }
+      return cartCopy;
+    }
+  
+    // cart : Array
+    // Item : Object/Class
+    // addItemToCart : Function
+    // removeItemFromCart : Function
+    // removeItemFromCartAll : Function
+    // clearCart : Function
+    // countCart : Function
+    // totalCart : Function
+    // listCart : Function
+    // saveCart : Function
+    // loadCart : Function
+    return obj;
+  })();
+
+  // Add item
+  $('.add-to-cart').click(function(event) {
+    event.preventDefault();
+    var name = $(this).data('name');
+    var price = Number($(this).data('price'));
+    shoppingCart.addItemToCart(name, price, 1);
+    displayCart();
+  });
+  
+  // Clear items
+  $('.clear-cart').click(function() {
+    shoppingCart.clearCart();
+    displayCart();
+  });
+  
+  function displayCart() {
+    var cartArray = shoppingCart.listCart();
+    var output = "";
+    for(var i in cartArray) {
+      output += "<tr>"
+        + "<td>" + cartArray[i].name + "</td>" 
+        + "<td>(" + cartArray[i].price + ")</td>"
+       
+        + "<td><button class='delete-item btn btn-danger' data-name=" + cartArray[i].name + ">X</button></td>"
+        + " = " 
+        + "<td>" + cartArray[i].total + "</td>" 
+        +  "</tr>";
+    }
+    $('.show-cart').html(output);
+    $('.total-cart').html(shoppingCart.totalCart());
+    $('.total-count').html(shoppingCart.totalCount());
+  }
+  
+  // Delete item button
+  
+  $('.show-cart').on("click", ".delete-item", function(event) {
+    var name = $(this).data('name')
+    shoppingCart.removeItemFromCartAll(name);
+    displayCart();
+  })
+
+  // Item count input
+  $('.show-cart').on("change", ".item-count", function(event) {
+     var name = $(this).data('name');
+     var count = Number($(this).val());
+    shoppingCart.setCountForItem(name, count);
+    displayCart();
+  });
+  
+  displayCart();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+  
+/* check login */
+
+function login(){
+        var user = document.getElementById('user').value;
+        var pass = document.getElementById('pass').value;
+        var passw= /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}/;
+
+  if(user == "" || pass == ""){
+          alert("sorry...Please Enter Username and Passowrd");
+     }
+     else{
+  if(pass.match(passw)){
+    location.href = "shopping.html";
+    alert("Thank you");
+    }
+    else{
+      alert("Password should be 6 characters and at least one number, one lowercase and one uppercase letter"); 
+  }
+       }
+           }
+  
+
+ function loginpage(){
+   return location.href = "login.html";
+ }
+
+ function register(){
+   var user=document.getElementById('Username').value;
+   var email=document.getElementById('email').value;
+   var pw=document.getElementById('password').value;
+   var cpw=document.getElementById('password_confirm').value;
+   var passw= /(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{6,}/;
+
+   if (user == "")                                  
+    { 
+        alert("Please insert your name"); 
+        user.focus();
+    }
+    else   
+    if (email == "")                                   
+    { 
+        alert("Please put a valid email address"); 
+        email.focus(); 
+       
+    } 
+    else   
+    if (email.indexOf("@", 0) < 0)                 
+    { 
+        alert("Please put a valid email address"); 
+        email.focus(); 
+    }
+    else
+    if(passw.test(pw)){
+      alert("Password should be 6 characters and at least one number, one lowercase and one uppercase letter"); 
+      }  
+      else
+      if(cpw != pw){
+        alert("check your confirm password please"); 
+        } 
+
+      else{
+        alert("Thank you for register ");
+      location.href = "login.html";
+      
+}
+   
+  
+ }
+
+ 
